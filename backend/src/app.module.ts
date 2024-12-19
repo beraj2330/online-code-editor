57d014c796bb781b1c4350ce33ccd11783a45110
Jeel Bera <jeelbera2330@gmail.com>
2024-12-19 14:50:07 +0530

    Removed unnecessary files and app controller and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CodeExecutionModule } from './code-execution/code-execution.module';
 
 @Module({
   imports: [CodeExecutionModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
